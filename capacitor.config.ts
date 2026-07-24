61361bb19fe90eee6c42b29d48f24ca4c5c42cc2
Jude Samways <judesamways@Judes-MacBook-Pro.local>
2026-07-24 19:24:00 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.judesamways.lightwatch',
+  appName: 'lightwatchapp',
+  webDir: 'dist'
+};
+
+export default config;
+
